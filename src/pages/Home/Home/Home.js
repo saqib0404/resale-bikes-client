7e39a2c78ba0cffce5d5dc53ebae93c9bb9ed9d1
Saqib Ahmad <you@example.com>
2022-11-24 11:29:20 +0600

    partners section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Partners from '../Partners/Partners';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Partners></Partners>
         </div>
     );
 };
--- src/pages/Home/Partners/Partners.js
+++ src/pages/Home/Partners/Partners.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import yamaha from '../../../assets/yamaha.png';
+import honda from '../../../assets/honda.png';
+import lifan from '../../../assets/lifan.png';
+import suzuki from '../../../assets/suzuki.png';
+import bajaj from '../../../assets/bajaj.png';
 
 const Partners = () => {
     return (
-        <div>
-            
-        </div>
+        <section className=' my-10'>
+            <h2 className='text-center text-3xl font-semibold'>Products From Companies We Allow</h2>
+            <div className=' grid gap-4 grid-cols-3 md:grid-cols-5 w-11/12 ml-auto mt-8'>
+                <img className='w-24 text-center' src={yamaha} alt="" />
+                <img className='w-32' src={honda} alt="" />
+                <img className='w-24' src={lifan} alt="" />
+                <img className='w-24' src={suzuki} alt="" />
+                <img className='w-24' src={bajaj} alt="" />
+            </div>
+        </section>
     );
 };
 
